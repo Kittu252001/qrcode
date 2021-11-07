12152cb143bc5accb5c337655eb198fdb5bed596
Saikrishna_alishala <saikrishna.alishala@gmail.com>
2021-11-07 21:13:29 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: QrImage(
+            data: 'The Obstacle is the way',
+            version: QrVersions.auto,
+            size: 200,
+          ),
+        ),
+      ),
+    );
+  }
+}
